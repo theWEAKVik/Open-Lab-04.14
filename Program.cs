c2675ef08688cfd01a84c1d25c439f547f160ab4
Erik Zemčík <zemcik.erik@gmail.com>
2019-10-22 12:56:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_04._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

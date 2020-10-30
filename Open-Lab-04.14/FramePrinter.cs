3e98384a9c66599e6e6e8b597886f1c760d4b065
Viktor <krpectv57@gmail.com>
2020-10-30 18:13:09 +0100

    ono sa to stále rozbíjalo.

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,36 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            int h = 0;
+            for (int i = 0; i<strings.Length; i++)
+            {
+                if (strings[i].Length > h)
+                {
+                    h = strings[i].Length;
+                }
+            }
+            int g = h + 3;
+            //{ "Hello", "World", "in", "a", "frame" };6,9,
+            for (int i = 0; i < g + 1; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Console.Write("* " + strings[i]);
+                int d = h - strings[i].Length;
+                for (int b = 0; b < d; b++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" *");
+            }
+            for (int i = 0; i < g + 1; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
         }
     }
 }
